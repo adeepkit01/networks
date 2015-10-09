8ba4764c874769c2e3d9675fa93fc505f425f234
adeepkit01 <adadeepak8@gmail.com>
2015-10-09 23:58:22 +0530

    pushing code

---
+++ build/ns3/dhcp-module.h
@@ -0,0 +1,14 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_DHCP
+    
+
+// Module headers:
+#include "dhcp-helper.h"
+#include "dhcp-server.h"
+#include "dhcp-client.h"
+#include "dhcp-header.h"
+#endif
